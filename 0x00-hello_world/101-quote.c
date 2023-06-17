6a4876ad10714088aa5107bd61b4a65ffc57c6ad
Job3-eng <jobbunanga@gmail.com>
2023-06-17 07:40:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
 /**
  * main - main entry
  * Return : always 1 (sucess)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (1);
 }
