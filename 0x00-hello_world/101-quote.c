f14eb070e0216fd2c24b297dad506668a40f7f24
Job3-eng <jobbunanga@gmail.com>
2023-06-17 07:12:55 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - main entry
+ * Return : always 1 (sucess)
+ */
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+			return (1);
+}
