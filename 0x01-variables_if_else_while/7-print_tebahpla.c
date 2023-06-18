eecfe4076c8047c333d58d4c3c01dd534b456a67
Job3-eng <jobbunanga@gmail.com>
2023-06-18 06:37:15 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  * main - Prints the numbers since 0 to 9
+  *
+  * Return: Always (Success)
+  */
+int main(void)
+{
+	int a = 123;
+
+	while (a >= 97)
+	{
+		putchar(a);
+		a--;
+	}
+	putchar('\n');
+	return (0);
+}
