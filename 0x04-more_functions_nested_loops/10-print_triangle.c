fa0278df087c70d2e74d5cc608e4cbee7f6211d1
Job3-eng <jobbunanga@gmail.com>
2023-06-23 12:05:41 -0700

    10-print_triangle.c

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+*print_triangle - printing a triangle
+*@size:size of the triangle
+*Return:return is empty
+*/
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size > 0)
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for ((b = size - a); b > 0; b--)
+				_putchar(' ');		
+			for (b = 0; b < a; b++)
+				_putchar('#');
+			if (a == size)
+				continue;
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
