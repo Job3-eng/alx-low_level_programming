4eedc1ac17ab03d3b67ee843aecf730c8fe4f80c
Job3-eng <jobbunanga@gmail.com>
2023-06-23 09:19:13 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,24 +6,24 @@
   */
 void print_diagonal(int n)
 {
-        if (n <= 0)
-        {
-                _putchar('\n');
-        }
-        else
-        {
-                int a, b;
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int a, b;
 
-                for (a = 0; a < n; a++)
-                {
-                        for (b = 0; b < n; b++)
-                        {
-                                if (b == a)
-                                _putchar('\\');
-                                else if (b < a)
-                                        _putchar(' ');
-                        }
-                        _putchar('\n');
-                }
-        }
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b < n; b++)
+			{
+				if (b == a)
+					_putchar('\\');
+				else if (b < a)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
 }
