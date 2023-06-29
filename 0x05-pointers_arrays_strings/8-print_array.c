8a7251943781c46b810e10df46b65dd6c57d2e38
Job3-eng <jobbunanga@gmail.com>
2023-06-29 01:10:50 -0700

    8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_array- main entry
+ * @a: the integers
+ * @n: another integer
+ * Return: nothing
+ */
+void print_array(int *a, int n)
+{
+	int i_input;
+
+	for (i_input = 0; i_input < n; i_input++)
+	{
+		printf("%d", a[i_input]);
+		if (i_input != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+	putchar('\n');
+}
