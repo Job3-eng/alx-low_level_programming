cc21b3558d07733b0521cc3eb3463d5b156c4bb4
Job3-eng <jobbunanga@gmail.com>
2023-06-29 16:31:07 -0700

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+  * leet - Encoding
+  * @s: string
+  *
+  * Return: encoded string
+  */
+char *leet(char *s)
+{
+	int i = 0, j = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (s[i])
+	{
+		j = 0;
+
+		while (j < l)
+		{
+			if (s[i] == r[j] || s[i] - 32 == r[j])
+			{
+				s[i] = n[j];
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (s);
+}
