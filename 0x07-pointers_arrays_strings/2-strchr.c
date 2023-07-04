5cb3c57d3910afbd011ac4672e59eea2c77bfb02
Job3-eng <jobbunanga@gmail.com>
2023-07-04 14:12:16 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - prints  occurrence of a char.
+ * @s: source
+ * @c: char
+ *
+ * Return: string.
+ */
+char *_strchr(char *s, char c)
+{
+	do {
+		if (*s == c)
+		{
+			return (s);
+
+		}
+		else
+			s++;
+	} while (*s != '\0')
+	if (*s == c)
+		return (s);
+	else
+		return (0);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -35,3 +35,4 @@ void print_buffer(char *b, int size);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
