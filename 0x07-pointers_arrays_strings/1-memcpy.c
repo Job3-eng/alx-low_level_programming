2a536562400d74b1c564eb8b280e7e8be911c268
Job3-eng <jobbunanga@gmail.com>
2023-07-04 13:59:06 -0700

    1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _memcpy - fills memory
+ * @dest: source string
+ * @src: filling string
+ * @n: lenght
+ * Return: string.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+	{
+		*(dest + a) = *(src + a);
+	}
+	return (dest);
+}
