44eb3bd000d2870f103d3fbe6388f5be78815f77
Job3-eng <jobbunanga@gmail.com>
2023-07-06 15:41:35 -0700

    100-is_palindrome.c

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,53 @@
+#include "main.h"
+/**
+ * s_len - calculates length
+ * @s: string
+ * Return: string.
+ */
+int s_len(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1 + s_len(s + 1));
+	}
+}
+/**
+ * palindrome - check if a string is a palindrome.
+ * @s: string
+ * @a: point of recursion
+ * @length: length of the string
+ * Return: 1 if is palindrome otherwise 0.
+ */
+int palindrome(char *s, int a, int length)
+{
+	if (*(s + a) == *(s + length - 1 - a) && a == (length / 2))
+	{
+		return (1);
+	}
+	else if (*(s + a) != *(s + length - 1 - a))
+	{
+		return (0);
+	}
+	else
+	{
+		return (palindrome(s, a + 1, length));
+	}
+}
+/**
+ * is_pali - checking for a palindrome.
+ * @s: string
+ *
+ *
+ * Return: 1 if palindrome otherwise 0.
+ */
+int is_pali(char *s)
+{
+	if (palindrome(s, 0, s_length(s)) == 1)
+		return (1);
+	else
+		return (0);
+}
