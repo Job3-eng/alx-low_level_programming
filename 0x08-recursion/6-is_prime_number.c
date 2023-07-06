550df928f8607b412b4da6fc0f1c94257a5f2285
Job3-eng <jobbunanga@gmail.com>
2023-07-06 15:25:31 -0700

    6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,11 +3,11 @@
  * is_prime_number- checking for prime number
  * @n: number
  * Return: prime number
+ * int is_prime_number(int n)
+ * {
+ * return (checking_prime(n, 1));
+ * }
  */
-int is_prime_number(int n)
-{
-	return (checking_prime(n, 1));
-}
 /**
  * checking_prime- checking if number is prime
  *
@@ -24,4 +24,13 @@ int checking_prime(int n, int a)
 	if ((n / a) < a)
 		return (1);
 	return (checking_prime(n, a + 1));
+}
+/**
+ * is_prime_number- checking for prime number
+ * @n: number
+ * Return: prime number
+ */
+int is_prime_number(int n)
+{
+	return (checking_prime(n, 1));
 }
