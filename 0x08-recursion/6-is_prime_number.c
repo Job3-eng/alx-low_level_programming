5de5565e61a62ad61682f8f72021fc6907d2a1c5
Job3-eng <jobbunanga@gmail.com>
2023-07-06 02:35:58 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * is_prime_number- checking for prime number
+ * @n: number
+ * Return: prime number
+ */
+int is_prime_number(int n)
+{
+	return (checking_prime(n, 1));
+}
+/**
+ * checking_prime- checking if number is prime
+ *
+ * @n: number
+ * @a: devisor
+ * Return: prime number
+ */
+int checking_prime(int n, int a)
+{
+	if (n <= 1)
+		return (0);
+	if (n % a == 0 && a > 1)
+		return (0);
+	if ((n / a) < a)
+		return (1);
+	return (checking_prime(n, a + 1));
+}
