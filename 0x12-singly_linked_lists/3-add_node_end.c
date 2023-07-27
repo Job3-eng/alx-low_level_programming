62ecf40af7cbf9f87afece59fcbbcf6b3b1ccc82
Job3-eng <jobbunanga@gmail.com>
2023-07-27 16:22:20 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -3,16 +3,15 @@
 #include "lists.h"
 
 /**
- * add_node_end - addng a new node to the linked list
+ * add_node - adding a new node
  * @head: double pointer
- * @str: string
+ * @str: new string
  *
- * Return: address of element
+ * Return: the address
  */
-list_t *add_node_end(list_t **head, const char *str)
+list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_s;
-	list_t *temp = *head;
 	unsigned int length = 0;
 
 	while (str[length])
@@ -24,19 +23,9 @@ list_t *add_node_end(list_t **head, const char *str)
 
 	new_s->str = strdup(str);
 	new_s->length = length;
-	new_s->next = NULL;
+	new_s->next = (*head);
+	(*head) = new_s;
 
-	if (*head == NULL)
-	{
-		*head = new_s;
-		return (new_s);
-	}
-
-	while (temp->next)
-		temp = temp->next;
-
-	temp->next = new_s;
-
-	return (new_s);
+	return (*head);
 }
 
