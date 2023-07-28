ce69f827dffef92ffca2d50a9ce1a5aa0e2b613b
Job3-eng <jobbunanga@gmail.com>
2023-07-28 14:41:49 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,48 +1,58 @@
-#include <stdlib.h>
-#include <string.h>
 #include "lists.h"
+#include <string.h>
+#include <stdio.h>
 
+list_t *new_node(const char *str);
 /**
- * add_node_end - adding a new node
- * @head: double pointer
+ * add_node_end - addnew node.
+ * @head: pointer of  list_t
  * @str: string
  *
- * Return: address
+ * Return: pointer
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	char *dup;
-	int length;
-	list_t *new_s, *last_s;
+	list_t *new_s;
 
-	new_s = malloc(sizeof(list_t));
-	if (new_s == NULL)
-		return (NULL);
-
-	dup = strdup(str);
-	if (str == NULL)
+	if (!(*head))
 	{
-		free(new_s);
-		return (NULL);
+		*head = new_node(str);
+		return (*head);
 	}
 
-	for (length = 0; str[length];)
-		length++;
-
-	new_s->str = dup;
-	new_s->length = length;
-	new_s->next = NULL;
-
-	if (*head == NULL)
-		*head = new_s;
-
+	if (!(*head)->next)
+	{
+		new_s = new_node(str);
+		new_s->next = (*head)->next;
+		(*head)->next = new_s;
+	}
 	else
-	{
-		last_s = *head;
-		while (last_s->next != NULL)
-			last_s = last_s->next;
-		last_s->next = new_s;
-	}
+		add_node_end(&(*head)->next, str);
 
 	return (*head);
 }
+
+/**
+ * new_node - create a new string.
+ * @str: string
+ *
+ * Return: pointer
+ */
+list_t *new_node(const char *str)
+{
+	list_t *new_s;
+	size_t length;
+
+	new_s = malloc(sizeof(list_t));
+	if (!new_s)
+		return (NULL);
+	length = 0;
+	while (str[length])
+		length++;
+
+	new_s->str = strdup(str);
+	new_s->length = length;
+	new_s->next = NULL;
+	return (new_s);
+}
+
