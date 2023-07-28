63520c38d26c61067518dbdceefde455d61544ab
Job3-eng <jobbunanga@gmail.com>
2023-07-28 15:29:59 -0700

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -10,32 +10,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new_s;
 	list_t *temp = *head;
 
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
-	new = malloc(sizeof(list_t));
+	new_s = malloc(sizeof(list_t));
 
-	if (!new)
+	if (!new_s)
 		return (NULL);
 
-	new->str = strdup(str);
-	new->len = len;
-	new->next = NULL;
+	new_s->str = strdup(str);
+	new_s->len = len;
+	new_s->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new;
-		return (new);
+		*head = new_s;
+		return (new_s);
 	}
 
 	while (temp->next)
 		temp = temp->next;
-	temp->next = new;
+	temp->next = new_s;
 
-	return (new);
+	return (new_s);
 }
 
