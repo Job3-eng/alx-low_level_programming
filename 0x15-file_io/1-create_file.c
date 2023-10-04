ab5ac8c698edd4304b703f5877b38cfbcd01228d
Job3-eng <jobbunanga@gmail.com>
2023-10-04 09:19:43 -0700

    1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * create_file - Creating file.
+ * @filename: A pointer pointer  to the file  namebeing  created.
+ * @text_content: string pointer writting to into the file.
+ *
+ * Return:  -1 for failure and 1 for success.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, w, length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length];)
+			length++;
+	}
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(fd, text_content, length);
+
+	if (fd == -1 || w == -1)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
